c6bd7ece18d4cd43edc5c0e9f6d680e19a058da1
ganga-treselle <ganga.lakshminarayanaperumal@treselle.com>
2017-08-21 19:24:31 +0530

    sadsa
    asdasdasd

---
+++ pyspark_sample.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
